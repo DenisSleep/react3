4c215b1c80714b13f5179c940bf483645ac5863b
DenisSleep <160733810+DenisSleep@users.noreply.github.com>
2024-10-26 22:23:04 +0300

    Реакт 3

---
+++ src/components/CatalogComponent/Catalog.jsx
@@ -0,0 +1,17 @@
+import style from './Catalog.module.css'
+import { Card } from '../CardComponent/Card'
+
+export function Catalog() {
+    return (
+        <>
+            <div className={style.catalog_grid}>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </>
+    )
+}
